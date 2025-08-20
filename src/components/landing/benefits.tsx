3ac16e6abd8e8422764fa887b67af75527e97f5c
Daniel Yamaguchi <danielyamaguchi409@gmail.com>
2025-08-20 23:44:54 +0000

    inves de preto use um cinza chumbo

--- src/components/landing/benefits.tsx
+++ src/components/landing/benefits.tsx
@@ -64,7 +64,7 @@ export default function Benefits() {
                             transition={{ duration: 0.5, delay: index * 0.1 }}
                             className={cn(
                                 "border rounded-2xl p-8 shadow-lg transition-all duration-300 ease-in-out relative z-10",
-                                isDark ? "bg-black text-white" : "bg-card text-card-foreground",
+                                isDark ? "bg-zinc-900 text-white" : "bg-card text-card-foreground",
                                 "hover:!opacity-100 hover:scale-105 hover:-translate-y-4 hover:!z-50",
                                 "lg:[&:not(:first-child)]:-ml-12",
                                 "group-hover:opacity-60"
